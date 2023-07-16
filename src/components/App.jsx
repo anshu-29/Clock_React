54fdad44b40cc8729a5c62cb50517c1b43da3318
Aakansh Arora <79619761+anshu-29@users.noreply.github.com>
2023-07-17 01:49:53 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,28 @@ import React, { useState } from "react";
 
 function App() {
   const time = new Date().toLocaleTimeString();
+  const date = new Date().toLocaleDateString();
 
   const [currentTime, timeState] = useState(time);
+  const [currentDate,dateState] = useState(date);
 
   setInterval(Showtime, 1000);
+  setInterval(Showtime,1000);
 
   function Showtime() {
     const newTime = new Date().toLocaleTimeString();
     timeState(newTime);
+    const newDate = new Date().toLocaleDateString();
+    dateState(newDate)
   }
+
 
   return (
     <div className="container">
-      <h2>{currentTime}</h2>
-      <button onClick={Showtime}>Get Time</button>
+      <button onClick={Showtime}><p>The Date and Time is</p></button>
+      <h1>{currentTime}</h1>
+      <h2>{currentDate}</h2>
+      
     </div>
   );
 }
